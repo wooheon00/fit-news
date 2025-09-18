392c6bccdb4267fee9df65c7a45db636b280c2af
WooHeon <achldngjs@naver.com>
2025-09-18 13:35:37 +0900

    Init

---
+++ src/main/java/com/fitnews/fit_news/FitNewsApplication.java
@@ -0,0 +1,13 @@
+package com.fitnews.fit_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnews/fit_news/FitNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnews.fit_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
